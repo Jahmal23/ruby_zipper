c50bb97e411f7f791a804a02252f33b54589f5da
Jahmal Lewis <Jahmal23@gmail.com>
2016-09-19 12:55:46 -0500

    creating and seeding the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Name.create(last: 'Silva')
+Name.create(last: 'Santos')
+
+Name.create(last: 'Sousa')
+Name.create(last: 'Oliveira')
+
+Name.create(last: 'Lima')
+Name.create(last: 'Carvalho')
+
+Name.create(last: 'Ferreira')
+Name.create(last: 'Rodrigues')
+
+Name.create(last: 'Almeida')
+Name.create(last: 'Costa')
+
+Name.create(last: 'Gomes')
+Name.create(last: 'Araújo')
+
+Name.create(last: 'Martins')
+Name.create(last: 'Melo')
+
+Name.create(last: 'Barbosa')
+Name.create(last: 'Ribeiro')
+
+Name.create(last: 'Alves')
+Name.create(last: 'Cardoso')
+
+Name.create(last: 'Rocha')
+Name.create(last: 'Correia')
+
+Name.create(last: 'Teixeira')
+Name.create(last: 'Fernandes')
+
+Name.create(last: 'Montes')
+Name.create(last: 'Morais')
+
+Name.create(last: 'Gonçalves')
+
+
+
