0a228cba9f33fc2e3ad67c05b70a95de046238b4
Jahmal Lewis <Jahmal23@gmail.com>
2016-09-19 15:49:52 -0500

    adding bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,48 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# The data can then be loaded with the rake db:seed (or create!d alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   cities = City.create!([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create!(name: 'Emanuel', city: cities.first)
 
-Name.create(last: 'Silva')
-Name.create(last: 'Santos')
+Name.create!(last: 'Silva')
+Name.create!(last: 'Santos')
 
-Name.create(last: 'Sousa')
-Name.create(last: 'Oliveira')
+Name.create!(last: 'Sousa')
+Name.create!(last: 'Oliveira')
 
-Name.create(last: 'Lima')
-Name.create(last: 'Carvalho')
+Name.create!(last: 'Lima')
+Name.create!(last: 'Carvalho')
 
-Name.create(last: 'Ferreira')
-Name.create(last: 'Rodrigues')
+Name.create!(last: 'Ferreira')
+Name.create!(last: 'Rodrigues')
 
-Name.create(last: 'Almeida')
-Name.create(last: 'Costa')
+Name.create!(last: 'Almeida')
+Name.create!(last: 'Costa')
 
-Name.create(last: 'Gomes')
-Name.create(last: 'Araújo')
+Name.create!(last: 'Gomes')
+Name.create!(last: 'Araújo')
 
-Name.create(last: 'Martins')
-Name.create(last: 'Melo')
+Name.create!(last: 'Martins')
+Name.create!(last: 'Melo')
 
-Name.create(last: 'Barbosa')
-Name.create(last: 'Ribeiro')
+Name.create!(last: 'Barbosa')
+Name.create!(last: 'Ribeiro')
 
-Name.create(last: 'Alves')
-Name.create(last: 'Cardoso')
+Name.create!(last: 'Alves')
+Name.create!(last: 'Cardoso')
 
-Name.create(last: 'Rocha')
-Name.create(last: 'Correia')
+Name.create!(last: 'Rocha')
+Name.create!(last: 'Correia')
 
-Name.create(last: 'Teixeira')
-Name.create(last: 'Fernandes')
+Name.create!(last: 'Teixeira')
+Name.create!(last: 'Fernandes')
 
-Name.create(last: 'Montes')
-Name.create(last: 'Morais')
+Name.create!(last: 'Montes')
+Name.create!(last: 'Morais')
 
-Name.create(last: 'Gonçalves')
+Name.create!(last: 'Gonçalves')
 
 
 
