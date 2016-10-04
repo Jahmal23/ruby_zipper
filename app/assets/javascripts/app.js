29e8923a1b8a1a8d1059580fa492067a702faf1c
Jahmal Lewis <Jahmal23@gmail.com>
2016-10-04 15:02:04 -0500

    angular is hitting the rails api

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,6 +1,5 @@
-var zipper;
 
-zipper = angular.module('zipper', ['templates', 'ngRoute', 'ngResource', 'controllers']);
+var zipper = angular.module('zipper', ['templates', 'ngRoute', 'ngResource', 'controllers']);
 
 zipper.config([
     '$routeProvider', function($routeProvider) {
@@ -11,21 +10,28 @@ zipper.config([
     }
 ]);
 
-var controllers;
-
-controllers = angular.module('controllers', []);
+var controllers = angular.module('controllers', []);
 
 controllers.controller("SearchController", ['$scope', '$routeParams', '$location', '$resource',
                         function($scope, $routeParams, $location, $resource) {
 
+                            $scope.search = function(zip) {
+                                return $location.path("/").search('zip', zip);
+                            };
 
+                            var persons = $resource('/search/:zip', {
+                                zip: "@zip"
+                            });
 
-
-
-                        }
-
-
-
-
-
-                        ]);
+                            if ($routeParams.zip) {
+                                persons.query({
+                                    zip: $routeParams.zip
+                                }, function(results) {
+                                    return $scope.persons = results;
+                                });
+                            }
+                            else
+                            {
+                                $scope.persons = [];
+                            }
+                        }]);
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,12 +6,13 @@ class SearchController < ApplicationController
     names_to_search = NameService.new.get_searchable_names
 
     if Rails.env.development?
-      search_result_json =  ZipCodeSearchService.fake_white_page_search_results
+      search_result_json_list = []
+      search_result_json_list << ZipCodeSearchService.fake_white_page_search_results
     else
-      search_result_json =  ZipCodeSearchService.new.get_zip_code_last_name_results(params[:zip], names_to_search)
+      search_result_json_list =  ZipCodeSearchService.new.get_zip_code_last_name_results(params[:zip], names_to_search)
     end
 
-    @persons = PersonService.new.get_persons_from_json_list(search_result_json)
+    @persons = PersonService.new.get_persons_from_json_list(search_result_json_list)
 
     render json: @persons.to_json
   end
